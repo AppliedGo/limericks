783ba9534a65b8e1b90921e658e2a03ec297e604
christophberger <christophberger@users.noreply.github.com>
2022-12-08 13:29:26 +0100

    feat: new article 'limericks'

--- limericks.go
+++ limericks.go
@@ -19,52 +19,248 @@ Comments and code in this file are used for describing and explaining a particul
 -->
 
 +++
-title = ""
+title = "Poetry time! Go proverbs as limericks"
 description = ""
 author = "Christoph Berger"
 email = "chris@appliedgo.net"
-date = "2017-00-00"
-draft = "true"
-categories = [""]
-tags = ["", "", ""]
-articletypes = ["Tutorial"]
+date = "2022-12-08"
+draft = "false"
+categories = ["Patterns and Paradigms"]
+tags = ["proverbs", "best-practices", ""]
+articletypes = ["Odds And Ends"]
 +++
 
-### Summary goes here
+
+The Go proverbs capture the essence of Go. Too concise for your taste? No worries, here is each proverb explained in a limerick.
 
 <!--more-->
 
-## Intro goes here
+> With a (not so) tiny bit of help from ChatGPT.
+>
+> My sincere apologies to Edward Lear.
 
-## The code
-*/
+# Go Proverbs
+*Simple, Poetic, Pithy*
 
-// ## Imports and globals
-package main
+## Don't communicate by sharing memory, share memory by communicating.
 
-/*
-## How to get and run the code
+Once there was a Go programmer\
+Whose code was quite a bummer\
+But he followed the rules\
+Of the Go proverbs, and jewels\
+Of wisdom were soon discovered.
 
-Step 1: `go get` the code. Note the `-d` flag that prevents auto-installing
-the binary into `$GOPATH/bin`.
+## Concurrency is not parallelism.
 
-    go get -d github.com/appliedgo/TODO:
+There once was a programmer named Bress,\
+Who thought concurrency was best,\
+But parallelism he found,\
+Was a completely different ground,\
+And his code finally ran with finesse.
 
-Step 2: `cd` to the source code directory.
+## Channels orchestrate; mutexes serialize.
 
-    cd $GOPATH/src/github.com/appliedgo/TODO:
+In Go, channels are quite grand,\
+They help processes take a stand,\
+While mutexes will make,\
+Things run in a straight line,\
+And keep the program in command.
 
-Step 3. Run the binary.
+## The bigger the interface, the weaker the abstraction.
 
-    go run TODO:.go
+<!-- There once was an interface so big\
+It caused the abstraction to rig\
+The bigger it grew\
+The weaker it threw\
+Until it collapsed with a thud.-->
+
+There once was an interface big and wide\
+It was quite the spectacle, oh how it tried\
+But alas, it was weak\
+For its abstraction was bleak\
+Less is more, it's time to step aside
+
+## Make the zero value useful.
+
+There once was a zero value so bland\
+It sat there with nothing in hand\
+But one clever programmer\
+Gave it a new demeanor\
+Now it's useful and doing as planned.
+
+## interface{} says nothing.
+
+There once was a type named interface{}\
+That didn't reveal anything concise\
+It kept things quite vague\
+And left programmers in a rage\
+"What does this mean?" they would entice.
+
+## Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.
+
+<!-- Gofmt's style may not be the best\
+But it's still the tool that they request\
+Though it's not quite their taste\
+They'll use it with haste\
+For its formatting they do attest. -->
+
+Though Gofmt's style may not please\
+It's still a tool that we seize\
+To format our code\
+In a consistent mode\
+And make our projects look sleek and concise.
+
+## A little copying is better than a little dependency.
+
+A little copying's not a crime\
+It can save you from dependency's grime\
+Just be sure to give credit\
+Where credit is due\
+And avoid any potential legal time.
+
+## Syscall must always be guarded with build tags.
+
+<!-- Syscall must always be tagged\
+To avoid builds that are jagged and saggy\
+Guard it with care\
+And your code will be fair\
+And your builds will be smooth and swanky. -->
+
+Syscall must be guarded with care\
+With build tags to protect and ensnare\
+Any potential threat\
+That might cause it to fret\
+And keep your program running fair.
+
+## Cgo must always be guarded with build tags.
+
+Cgo must be guarded with tags\
+To prevent any potential snags\
+In the building process\
+It can save you from stress\
+And keep your program free of flags.
+
+<!-- Cgo must be used with tact\
+And guarded with build tags to act\
+As a shield from harm\
+And keep your program from alarm\
+So it can function without any lack. -->
+
+## Cgo is not Go.
+
+<!-- Cgo is not the same as Go\
+It's a different beast, you know\
+With unique features\
+And different creatures\
+So be sure to use it with a steady flow. -->
+
+Cgo may seem like Go at first glance\
+But it's quite a different circumstance\
+It allows for C code\
+To be integrated, bestowed\
+But it's still not the same at a glance.
+
+## With the unsafe package there are no guarantees.
+
+<!-- The unsafe package is quite the wild card\
+It offers no guarantees, quite hard\
+To use it with care\
+And be aware\
+Of the potential danger, be on guard. -->
+
+The unsafe package, it's plain to see\
+Comes with no guarantees, can't you agree?\
+It opens up doors\
+To potential pitfalls and roars\
+So use it cautiously, wisely, and free.
+
+## Clear is better than clever.
+
+Clever code can be quite a feat\
+But clear code can't be beat\
+It's easy to read\
+And doesn't mislead\
+So make sure your code is both clear and neat.
+
+<!-- Clever code may be a delight\
+But clear code is a better sight\
+It's easy to read\
+And won't leave you misled\
+So aim for clarity, not just insight. -->
+
+## Reflection is never clear.
+
+Reflection can be quite a fuss\
+It's never clear, that much is a must\
+It can cause confusion\
+And lead to an illusion\
+So use it sparingly, if at all, and adjust.
+
+## Errors are values.
+
+<!-- Errors are not just a message\
+They're values that we must acknowledge\
+They can provide insight\
+Into what's not quite right\
+So don't ignore them, let them collage.
+
+Errors are values, that much is true\
+They can be assigned and passed through\
+They provide insight\
+On what went wrong, in sight\
+And allow for graceful recovery, anew. -->
+
+Errors, they're not just a blight\
+But values that must be dealt with right\
+They can be stored\
+And later explored\
+To help your program take flight.
+
+## Don't just check errors, handle them gracefully.
+
+Checking errors is just the start\
+You must handle them with a graceful heart\
+Don't just ignore them\
+Or let your program condemn\
+Be mindful of how you play your part.
+
+<!-- Errors are values, that much is true\
+And they can be quite useful, that's no small feat\
+They can inform us\
+Of potential problems and fuss\
+So handle them gracefully and sweet. -->
 
 
-## Odds and ends
-## Some remarks
-## Tips
-## Links
+## Design the architecture, name the components, document the details.
+
+Design the architecture, with care\
+Name the components, with flair\
+Document the details\
+So others won't fail\
+And your project can thrive beyond compare.
 
 
-**Happy coding!**
+## Documentation is for users.
 
+Documentation, it's a must\
+For users, so they won't be lost\
+It helps them understand\
+And follow your plan\
+So they can use your program without any fuss.
+
+
+## Don't panic.
+
+When panic strikes, don't despair\
+Take a deep breath, and take the air\
+Remember that quote\
+And all will be afloat\
+"Don't panic," just breathe, and repair.
+
+## <!-- to add vertical space -->
+
+Happy coding!
+
+ʕ◔ϖ◔ʔ
+___
 */
